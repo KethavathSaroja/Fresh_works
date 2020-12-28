69b3b3cce1bab2552c2950ef7132d5a8530f39a8
Kethavath Saroja <76644988+KethavathSaroja@users.noreply.github.com>
2020-12-29 00:02:08 +0530

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -1,8 +1,7 @@
 import unittest
-#import code
 import actualcode as freshworks
 class TestMethod(unittest.TestCase):
-   # f=freshworks()
+  
     def test_create(self):
         self.assertEqual(freshworks.flag(),1)
     def test_read(self):
