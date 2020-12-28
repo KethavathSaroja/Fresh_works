63fff0b43dd2af094e20904ee4fc1f0a3c4a77f8
Kethavath Saroja <76644988+KethavathSaroja@users.noreply.github.com>
2020-12-29 00:00:13 +0530

    Update and rename freshworks.py to actual_code.py

--- actual_code.py
+++ actual_code.py
@@ -2,14 +2,14 @@ import threading
 from threading import*
 import time
 
-d={} 
+dict={} 
 
 def create(key,value,timeout=0):
-    if key in d:
+    if key in dict:
         print("error: this key already")
     else:
         if key.isalpha():
-            t=len(d)
+            t=len(dict)
             #print(t,value)
             #print(type(value))
             v=int(value)
@@ -19,7 +19,7 @@ def create(key,value,timeout=0):
                 else:
                     l=[value,time.time()+timeout]
                 if len(key)<=32: 
-                    d[key]=l
+                    dict[key]=l
             else:
                 print("error: Memory limit exceeded!! ")
         else:
@@ -32,10 +32,10 @@ def flag():
 #use syntax "read(key_name)"
             
 def read(key):
-    if key not in d:
+    if key not in dict:
         print("error: given key does not exist in database. Please enter a valid key") #error message4
     else:
-        b=d[key]
+        b=dict[key]
         if b[1]!=0:
             if time.time()<b[1]: #comparing the present time with expiry time
                 stri=str(key)+":"+str(b[0]) #to return the value in the format of JasonObject i.e.,"key_name:value"
@@ -54,42 +54,42 @@ def read(key):
 #use syntax "delete(key_name)"
 
 def delete(key):
-    if key not in d:
+    if key not in dict:
         print("error: given key does not exist in database. Please enter a valid key") #error message4
     else:
-        b=d[key]
+        b=dict[key]
         if b[1]!=0:
             if time.time()<b[1]: #comparing the current time with expiry time
-                del d[key]
+                del dict[key]
                 print("key is successfully deleted")
             else:
                 print("error: time-to-live of",key,"has expired") #error message5
         else:
-            del d[key]
+            del dict[key]
             print("key is successfully deleted")
 
 
 def modify(key,value):
-    b=d[key]
+    b=dict[key]
     if b[1]!=0:
         if time.time()<b[1]:
-            if key not in d:
+            if key not in dict:
                 print("error: given key does not exist in database. Please enter a valid key") #error message6
             else:
                 l=[]
                 l.append(value)
                 l.append(b[1])
-                d[key]=l
+                dict[key]=l
         else:
             print("error: time-to-live of",key,"has expired") #
     else:
-        if key not in d:
+        if key not in dict:
             print("error: given key does not exist in database. Please enter a valid key") #error message6
         else:
             l=[]
             l.append(value)
             l.append(b[1])
-            d[key]=l
+            dict[key]=l
 create("saroja",21)
 create("saroja",20,100) 
 
